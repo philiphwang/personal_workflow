7b561e4083ba1b494e41faa8a462ec25828bd438
Philip Hwang <philip@hwangnet.com>
2016-01-17 02:54:16 -0800

    initial maven setup

---
+++ com.hwangnet.projects.workflow/src/main/java/com/hwangnet/projects/App.java
@@ -0,0 +1,13 @@
+package com.hwangnet.projects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
